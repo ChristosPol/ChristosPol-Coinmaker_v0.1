b07e3b0f4d4ef166004a57aa13eca3be6a6be7e1
ChristosPol <christos.polysopoulos@gmail.com>
2020-07-19 18:28:22 +0200

    cleaning

--- Testing Module/01 API Connect.R
+++ Testing Module/01 API Connect.R
@@ -3,9 +3,6 @@
 # screen -S pull_rest_eth_data R
 .rs.restartR()
 rm(list = ls())
-options(digits=22)
-options(scipen=999)
-library(nanotime)
 source(paste("/media/chris/DATA/Documents/Bot_Trading/Coinmaker", "10 Utils.R", sep = "/"))
 setDTthreads(1)
 
@@ -19,12 +16,6 @@ API_Sign <- as.character(api_info$API_Sign)
 
 # Choose pair
 pair <- "XETHZEUR"
-# pair <- "BTCEUR"
-# pair <- "QTUMEUR"
-# pair <- "ETCEUR"
-# # pair <- "ZECEUR"
-# pair <- "EURUSD"
-# pair <- "TRXEUR"
 
 # Path to save results0
 data_path <- "/media/chris/DATA/Documents/Bot_Trading/Historical_data"
@@ -36,37 +27,11 @@ dir.create(paste(data_path, pair, sep ="/"), showWarnings = FALSE)
 pair_data_results <- paste(data_path, pair, sep ="/")
 
 # Choose initial ID for the first pull
-initial_id <- 1592985088058073687
-
-
-
-hist_trades_pair(sleep = 3, hist_id = initial_id, pair = pair)
-
-library(bit64)
-options("width"=60)
-
+options("width" = 60)
 v <- nanotime(Sys.time() - as.difftime(60, unit = "days"))
-initial_id <- as.integer64( v )
-as.numeric(as.character(initial_id))
+initial_id <- as.integer64(v)
 
-    url <- paste0('https://api.kraken.com/0/public/Trades?pair=',pair,'&since='
-                  ,as.numeric(initial_id))
-    dat <- jsonlite::fromJSON(url)
-temp <- cbind(data.frame(dat$result[1]), dat$result$last)
+initial_id <-as.numeric(as.character(initial_id))
 
-
-temp$Date_POSIXct <- anytime(as.numeric(as.character(temp$XETHZEUR.3)))
-temp$Time <- strftime(temp$Date_POSIXct, format = "%H:%M:%S")
-
-colnames(temp) <- c("price", "volume", "epoch_time", "buy_sell", "market_limit",
-                     "miscellaneous", "last_id", "Date_POSIXct", "Time")
-View(temp)
-frame[, Date := as.Date(Date_POSIXct)]
-frame[, Hour := substr(frame$Time, 1,5)]
-frame[, miscellaneous := NULL]
-View(temp)
-plot(as.numeric(as.character(temp$price)))
-set up the historical trades, accumulate and summarise demand and supply by summarizing volume by buy 
- and sell
- temp%>%group_by(buy_sell)%>% summarise(sum(as.numeric(volume)))
- 
+# Pull historical trades since initial id from epoch time
+hist_trades_pair(sleep = 3, hist_id = initial_id, pair = pair)
