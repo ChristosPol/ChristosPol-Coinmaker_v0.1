7273489ca398107955585017ca19e2d89d4a559b
ChristosPol <christos.polysopoulos@gmail.com>
2020-07-24 23:55:58 +0200

    setting up Dynamic SR lines

--- 10 Utils.R
+++ 10 Utils.R
@@ -63,28 +63,42 @@ plot_candlesticks <- function(dta, Ns, asset){
 }
 
 # Plot chart with SR lines and return values 
-SR_lines <- function(roll, data){
+SR_lines <- function(roll, data, plot.it){
   
   last_close <- data$close[nrow(data)]
   resistance <- unique(rollapplyr(data$close[-nrow(data)], roll, max, fill = NA))
+  resistance <- resistance[!is.na(resistance)]
   
   if (length(which(resistance < last_close)) == 0) {
     resistance_act <- min(resistance, na.rm = T)
+  
+  } else if(all(last_close > resistance)){
+    
+    resistance_act <- max(resistance, na.rm =T)
+    
+      
   } else {
     resistance_act <- min(resistance[-which(resistance < last_close)], na.rm = T)
   }
   
   support <- unique(rollapplyr(data$close[-nrow(data)], roll, min, fill = NA))
+  support <- support[!is.na(support)]
   
   if (length(which(support > last_close)) == 0) {
     support_act <- max(support, na.rm =T)
+    
+  } else if(all(last_close < support)){
+    
+    support_act <- min(support, na.rm =T)
+    
   } else {
     support_act <- max(support[-which(support > last_close)], na.rm = T)
   }
-  
-  plot_candlesticks(dta = data, Ns = nrow(data), asset = pair)
-  abline(h = resistance_act, col ="lightblue")
-  abline(h = support_act, col ="black")
+  if(plot.it == TRUE){
+    plot_candlesticks(dta = data, Ns = nrow(data), asset = pair)
+    abline(h = resistance_act, col ="lightblue")
+    abline(h = support_act, col ="black")
+  }
   return(list(SL = support_act, RL = resistance_act))
 }
 
--- Testing Module/Resistance_support.R
+++ Testing Module/Resistance_support.R
@@ -1,5 +1,5 @@
 candles
-roll <- 100
+roll <- 600
 # candles <- simple_OHLC(interval = 30, pair = "REPEUR")
 # candles$RSI <- RSI(candles$close, n =14)
 real <- ceiling(nrow(candles) / 10)
@@ -7,35 +7,162 @@ realized_candles <- candles[1:real, ]
 
 
 
-SL_lines <- function(roll, data){
-  
-  last_close <- data$close[nrow(data)]
-  resistance <- unique(rollapplyr(data$close[-nrow(data)], roll, max, fill = NA))
-  
-  if (length(which(resistance < last_close)) == 0) {
-    resistance_act <- min(resistance, na.rm = T)
-  } else {
-    resistance_act <- min(resistance[-which(resistance < last_close)], na.rm = T)
-  }
-  
-  support <- unique(rollapplyr(data$close[-nrow(data)], roll, min, fill = NA))
-  
-  if (length(which(support > last_close)) == 0) {
-    support_act <- max(support, na.rm =T)
-  } else {
-    support_act <- max(support[-which(support > last_close)], na.rm = T)
-  }
-  
-  plot_candlesticks(dta = data, Ns = nrow(data), asset = pair)
-  abline(h = resistance_act, col ="lightblue")
-  abline(h = support_act, col ="black")
-  return(list(SL = support_act, RL = resistance_act))
-}
-SL_lines(roll = 500, data=candles)
+SR_lines(roll = 50, data=realized_candles, plot.it = T)
 
 # Test, same 
 future_candles <- candles[(real + 1):nrow(candles), ]
+roll <- 800
+takeprofit <- 0.03
+stoploss_trail <- 0.01
+stoploss_ult <- 0.01
+initial_budget <- 500
+Pure_RSI_Volume_Trailing <- function(roll, takeprofit, stoploss_trail,stoploss_ult) {
+  
+  # Train and test datasets
+  realized_candles[, c("SL",
+                       "RL",
+                       "exit_condition",
+                       "crossover_support",
+                       "crossover_resistance",
+                       "action",
+                       "Units",
+                       "Price",
+                       "tp",
+                       "ult_sl",
+                       "trail_sl",
+                       "id") := list(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA)]
 
+  future_candles[, c("SL",
+                     "RL",
+                     "exit_condition",
+                     "crossover_support",
+                     "crossover_resistance",
+                     "action",
+                     "Units",
+                     "Price",
+                     "tp",
+                     "ult_sl",
+                     "trail_sl",
+                     "id") := list(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA)]
+  # nrow(future_candles)
+  # Going intro the loop for test data -----------------------------------------
+  for (i in 1:nrow(future_candles)){
+    
+    fut <- rbind(realized_candles, future_candles[i, ])
+    
+    # Add SR lines -------------------------------------------------------------
+    SR_vals <- SR_lines(roll = roll, data = fut, plot.it = FALSE)
+  
+    fut$SL[nrow(fut)] <- SR_vals$SL
+    fut$RL[nrow(fut)] <- SR_vals$RL
+    
+    # Technical indicators -----------------------------------------------------
+    
+    # Support and resistance lines
+    fut$crossover_support[nrow(fut)] <- ifelse(fut$close[nrow(fut)] <= fut$SL[nrow(fut)],
+                                              "below_support", "above_support")
+  
+    fut$crossover_resistance[nrow(fut)] <- ifelse(fut$close[nrow(fut)] >= fut$RL[nrow(fut)],
+                                               "above_resistance", "below_resistance")
+    
+    
+    # Exit condition for takeprofit  - Fixed
+    tp <- tail(fut$close[fut$action == "buy"][!is.na(fut$close[fut$action == "buy"])], 1) + takeprofit * tail(fut$close[fut$action == "buy"][!is.na(fut$close[fut$action == "buy"])], 1)
+    
+    if (length(tp) == 0) {
+      tp <- 0
+    }
+    
+    # Ultimate stop loss
+    ult_sl <- tail(fut$close[fut$action == "buy"][!is.na(fut$close[fut$action == "buy"])], 1) - stoploss_ult * tail(fut$close[fut$action == "buy"][!is.na(fut$close[fut$action == "buy"])], 1)
+    
+    if (length(ult_sl) == 0) {
+      ult_sl <- 0
+    }
+    
+    
+    # Trailing stop loss
+    # browser()
+    if (fut$action[nrow(fut)-1] %in% c("buy", "keep") & ( fut$close[nrow(fut)] > fut$close[nrow(fut)-1] )  ){
+      
+      trail_sl <- fut$close[nrow(fut)] - stoploss_trail * fut$close[nrow(fut)]
+      if( trail_sl < tail(fut$trail_sl[!is.na(fut$trail_sl)], 1)){
+        trail_sl <- tail(fut$trail_sl[!is.na(fut$trail_sl)], 1)
+        
+      }else {
+        trail_sl <- fut$close[nrow(fut)] - stoploss_trail * fut$close[nrow(fut)]
+        
+      }
+      
+      
+    } else if (fut$action[nrow(fut)-1] %in% c("buy", "keep") & ( fut$close[nrow(fut)] <= fut$close[nrow(fut)-1] ) ){
+      
+      trail_sl <- tail(fut$trail_sl[!is.na(fut$trail_sl)], 1)
+    } else {
+      
+      trail_sl <-0
+    }
+    
+    
+    if(length(trail_sl) == 0 ){
+      
+      trail_sl <- 0
+    }
+    # browser()
+    
+    
+    
+    fut$tp[nrow(fut)] <- tp
+    fut$ult_sl[nrow(fut)] <- ult_sl
+    fut$trail_sl[nrow(fut)] <- trail_sl
+    
+    
+    fut$exit_condition[nrow(fut)] <- fut$trail_sl[nrow(fut)] > fut$close[nrow(fut)] | fut$ult_sl[nrow(fut)] > fut$close[nrow(fut)] | fut$tp[nrow(fut)] < fut$close[nrow(fut)]
+    
+    
+    # Deciding upon action -----------------------------------------------------
+    
+    # Buy condition
+    if ( (is.na(fut$action[nrow(fut) - 1]) |  fut$action[nrow(fut) - 1] %in% c("sell", "no action")) &
+         fut$crossover_support[nrow(fut)] == "below_support") {
+      
+      fut$action[nrow(fut)] <- "buy"
+      fut$Units[nrow(fut)] <- initial_budget / fut$close[nrow(fut)]
+      fut$Price[nrow(fut)] <- fut$Units[nrow(fut)] * fut$close[nrow(fut)]
+      fut$id[nrow(fut)] <- round(runif(1, 10000, 5000000))
+      
+      # Sell condition
+    } else if (fut$action[nrow(fut) - 1] %in% c("keep", "buy") & (
+      fut$exit_condition[nrow(fut)] == TRUE  )) {
+      
+      fut$action[nrow(fut)] <- "sell"
+      fut$Units[nrow(fut)] <- fut$Units[nrow(fut) -1]
+      fut$Price[nrow(fut)] <- fut$close[nrow(fut)]* fut$Units[nrow(fut)]
+      fut$id[nrow(fut)] <- fut$id[nrow(fut)-1]
+      initial_budget <- fut$Price[nrow(fut)]
+      
+      # Keep condition
+    } else if ( fut$action[nrow(fut) - 1] %in% c("buy", "keep")   & 
+                fut$exit_condition[nrow(fut)] == FALSE  ) {
+      
+      fut$action[nrow(fut)] <- "keep"
+      fut$Units[nrow(fut)] <- fut$Units[nrow(fut) -1 ]
+      fut$id[nrow(fut)] <- fut$id[nrow(fut)-1]
+      
+    } else {
+      
+      fut$action[nrow(fut)] <- "no action"
+      
+    }
+    
+    realized_candles <- fut
+    print(i)
+    # browser()
+  }
+  return(realized_candles)
+}
 
-candles$RSI
+View(realized_candles)
+warnings()
 
+unique(realized_candles$Price)
