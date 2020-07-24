78bdec38595579d84eb40592b18c627b41d6e4d9
ChristosPol <christos.polysopoulos@gmail.com>
2020-07-24 21:40:11 +0200

    SR lines function

--- 10 Utils.R
+++ 10 Utils.R
@@ -62,6 +62,33 @@ plot_candlesticks <- function(dta, Ns, asset){
   # axis(1, at = xs, labels = dta$time, las = 2)
 }
 
+# Plot chart with SR lines and return values 
+SR_lines <- function(roll, data){
+  
+  last_close <- data$close[nrow(data)]
+  resistance <- unique(rollapplyr(data$close[-nrow(data)], roll, max, fill = NA))
+  
+  if (length(which(resistance < last_close)) == 0) {
+    resistance_act <- min(resistance, na.rm = T)
+  } else {
+    resistance_act <- min(resistance[-which(resistance < last_close)], na.rm = T)
+  }
+  
+  support <- unique(rollapplyr(data$close[-nrow(data)], roll, min, fill = NA))
+  
+  if (length(which(support > last_close)) == 0) {
+    support_act <- max(support, na.rm =T)
+  } else {
+    support_act <- max(support[-which(support > last_close)], na.rm = T)
+  }
+  
+  plot_candlesticks(dta = data, Ns = nrow(data), asset = pair)
+  abline(h = resistance_act, col ="lightblue")
+  abline(h = support_act, col ="black")
+  return(list(SL = support_act, RL = resistance_act))
+}
+
+
 # Indicator trading ------------------------------------------------------------
 # MACD crossover going long
 
--- Testing Module/Resistance_support.R
+++ Testing Module/Resistance_support.R
@@ -1,27 +1,41 @@
 candles
 roll <- 100
-candles <- simple_OHLC(interval = 240, pair = "DASHEUR")
-candles$RSI <- RSI(candles$close, n =14)
-plot_candlesticks(dta = candles, Ns = nrow(candles), asset = pair)
+# candles <- simple_OHLC(interval = 30, pair = "REPEUR")
+# candles$RSI <- RSI(candles$close, n =14)
+real <- ceiling(nrow(candles) / 10)
+realized_candles <- candles[1:real, ]
 
-last_close <- candles$close[nrow(candles)]
-resistance <- unique(rollapplyr(candles$close, roll, max, fill = NA))
 
-if (length(which(resistance < last_close)) == 0) {
-  resistance_act <- resistance
-} else {
-  resistance_act <- resistance[-which(resistance < last_close)]
+
+SL_lines <- function(roll, data){
+  
+  last_close <- data$close[nrow(data)]
+  resistance <- unique(rollapplyr(data$close[-nrow(data)], roll, max, fill = NA))
+  
+  if (length(which(resistance < last_close)) == 0) {
+    resistance_act <- min(resistance, na.rm = T)
+  } else {
+    resistance_act <- min(resistance[-which(resistance < last_close)], na.rm = T)
+  }
+  
+  support <- unique(rollapplyr(data$close[-nrow(data)], roll, min, fill = NA))
+  
+  if (length(which(support > last_close)) == 0) {
+    support_act <- max(support, na.rm =T)
+  } else {
+    support_act <- max(support[-which(support > last_close)], na.rm = T)
+  }
+  
+  plot_candlesticks(dta = data, Ns = nrow(data), asset = pair)
+  abline(h = resistance_act, col ="lightblue")
+  abline(h = support_act, col ="black")
+  return(list(SL = support_act, RL = resistance_act))
 }
+SL_lines(roll = 500, data=candles)
 
-support <- unique(rollapplyr(candles$close, roll, min, fill = NA))
-
-if (length(which(support > last_close)) == 0) {
-  support_act <- support
-} else {
-  support_act <- support[-which(support > last_close)]
-}
+# Test, same 
+future_candles <- candles[(real + 1):nrow(candles), ]
 
 
-abline(h = resistance_act, col ="lightblue")
-abline(h = support_act, col ="grey")
 candles$RSI
+
